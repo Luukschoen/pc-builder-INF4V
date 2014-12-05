bcc59601b4d1a4406d5a201ea06ce9fdbef6926f
Luuk Schoenmakers <luuk.schoenmakers@gmail.com>
2014-12-05 16:30:22 +0100

    added example objects

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -3,6 +3,12 @@ app.controller('MainController', ['$scope', '$rootScope', '$http', '$state', '$s
     function MainController($scope, $rootScope, $http, $state, $stateParams, $filter) { 
         
     $scope.headlines = "Hi, ik ben hier gekomen via de controller!";
+    $scope.objects = [
+      {naam: "Mitchell Irvine", points: 3},
+      {naam: "Niels van 't Hof", points: 5},
+      {naam: "Luuk Schoenmakers", points: 4},
+      {naam: "Dennis Bergsma", points: 0}
+    ]
 
 
     }
