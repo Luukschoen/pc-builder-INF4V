6e648f58314b2a9d1808af07ef6db2804141a2df
Luuk Schoenmakers <luuk.schoenmakers@gmail.com>
2014-12-05 16:23:20 +0100

    added controller example

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,6 +2,7 @@
 app.controller('MainController', ['$scope', '$rootScope', '$http', '$state', '$stateParams', '$filter',
     function MainController($scope, $rootScope, $http, $state, $stateParams, $filter) { 
         
+    $scope.headlines = "Hi, ik ben hier gekomen via de controller!";
 
 
     }
