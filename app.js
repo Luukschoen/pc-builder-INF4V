49fe670a8db1d771fac18cba6bf8c692510aa64a
Luuk Schoenmakers <luuk.schoenmakers@gmail.com>
2014-12-05 15:09:30 +0100

    last pieces for our new project structure

---
+++ app.js
@@ -0,0 +1,14 @@
+var app = angular.module('app', ['ui.router']);
+/* This is the piece that defines the state providers. */
+app.config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise("/home");
+//Set up the states
+  $stateProvider
+  //routing for home states
+      .state('home', {
+      url: '/home',
+      templateUrl: "templates/content/home.html"
+    })
+});
+
+
---
+++ js/controllers/MainController.js
@@ -0,0 +1,9 @@
+
+app.controller('MainController', ['$scope', '$rootScope', '$http', '$state', '$stateParams', '$filter',
+    function MainController($scope, $rootScope, $http, $state, $stateParams, $filter) { 
+        
+
+
+    }
+]);
+
