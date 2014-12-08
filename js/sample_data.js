55af861ca5fa7adc1e91f84d7028c20edf50aba4
Luuk Schoenmakers <luuk.schoenmakers@gmail.com>
2014-12-08 15:12:22 +0100

    changes to gitignore. No more ds_store and bower_components in commits

--- js/sample_data.js
+++
@@ -1,8 +0,0 @@
-// dummy data
-motherbords = [
-{"price": ["\u20ac 215,00"], "stock": ["Product is op voorraad"], "link": ["http://www.informatique.nl/543088/msi-x99s-sli-plus.html"], "title": ["MSI X99S SLI PLUS"]},
-{"price": ["\u20ac 259,00"], "stock": ["Product is op voorraad"], "link": ["http://www.informatique.nl/543342/asus-x99-a.html"], "title": ["ASUS X99-A"]},
-{"price": ["\u20ac 370,00"], "stock": ["Product is op voorraad"], "link": ["http://www.informatique.nl/543184/asus-x99-deluxe.html"], "title": ["Asus X99-DELUXE"]},
-{"price": ["\u20ac 259,00"], "stock": ["Product is op voorraad"], "link": ["http://www.informatique.nl/543334/asrock-x99-extreme6.html"], "title": ["ASRock X99 Extreme6"]},
-{"price": ["\u20ac 105,00"], "stock": ["Product wordt geleverd op bestelling."], "link": ["http://www.informatique.nl/987461/gigabyte-ga-b85tn-s1150-b85-2xddr3.html"], "title": ["Gigabyte GA-B85TN S1150 B85 2xDDR3"]}
-]
